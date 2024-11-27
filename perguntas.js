c0e2d92fda94cb5084e0336b4323cbffba15ed56
3AGMN008 <00001062681885sp@al.educacao.sp.gov.br>
2024-11-27 09:10:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Geografia',
+    'Quais o menor e o maior país do mundo?',
+    'Vaticano e Rússia'
+)
+
+criaCartao(
+    'História',
+    'Qual o nome do presidente do Brasil que ficou conhecido como Jango?',
+    'João Goulart'
+)
+
+criaCartao(
+    'Química',
+    'Atualmente, quantos elementos químicos a tabela periódica possui?',
+    '118'
+)
+
+criaCartao(
+    'Inglês',
+    'O que a palavra legend significa em português?',
+    'Lenda'
+)
+
+criaCartao(
+    'Artes',
+    'Quem pintou A noite estrelada?',
+    'Vincent Van Gogh'
+)
+
+criaCartao(
+    'Mitologia',
+    'Júpiter e Plutão são os correlatos romanos de quais deuses gregos?',
+    'Zeus e Hades'
+)
+
+criaCartao(
+    'Filosofia',
+    'Qual filósofo disse Só sei que nada sei?',
+    'Sócrates'
+)
+
+criaCartao(
+    'Cinema',
+    'Qual música ganhou o Oscar de Melhor Canção Original por sua inclusão no filme Titanic?',
+    'My Heart Will Go On, de Celine Dion'
+)
+
+criaCartao(
+    'Literatura',
+    'Muitos escritores fizeram parte da primeira geração modernista no Brasil, exceto:',
+    'Carlos Drummond de Andrade'
+)
